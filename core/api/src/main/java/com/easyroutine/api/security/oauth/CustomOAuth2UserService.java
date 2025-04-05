aafea4bc37561a1e620c8fb8f60e61ce61872c88
lkwoung88 <lkwoung88@gmail.com>
2025-04-05 23:43:06 +0900

    [#1 프로젝트 세팅] chore: 프로젝트 구조 설정 및 시큐리티 초기 설정 (#2)
    * [chore] 멀티모듈 설정
    
    * [chore] monitoring, logging 설정파일 세팅
    
    * [feat] Oauth2 설정 (구글)
    
    * [chore] Member 샘플 코드 추가

---
+++ core/api/src/main/java/com/easyroutine/api/security/oauth/CustomOAuth2UserService.java
@@ -0,0 +1,36 @@
+package com.easyroutine.api.security.oauth;
+
+import com.easyroutine.api.security.oauth.response.GoogleResponse;
+import com.easyroutine.api.security.oauth.response.OAuth2Response;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+
+        OAuth2Response oAuth2Response = getOAuth2ResponseBy(registrationId, attributes);
+
+        // TODO : add other roles
+        return new CustomOAuth2User(oAuth2Response, "ROLE_USER");
+    }
+
+    private OAuth2Response getOAuth2ResponseBy(String registrationId, Map<String, Object> attributes) {
+        switch (registrationId) {
+            case "google":
+                return new GoogleResponse(attributes);
+            default:
+                throw new IllegalArgumentException("Unsupported OAuth2 provider: " + registrationId);
+        }
+    }
+}
