b7dfbf31817481bc1924b1894789a5eb53adc10a
lkwoung88 <lkwoung88@gmail.com>
2025-05-01 03:54:15 +0900

    [test] context api test code 작성

---
+++ core/api/src/test/java/com/easyroutine/ControllerTestSupport.java
@@ -0,0 +1,17 @@
+package com.easyroutine;
+
+import com.easyroutine.api.controller.ContextController;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = {ContextController.class})
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
