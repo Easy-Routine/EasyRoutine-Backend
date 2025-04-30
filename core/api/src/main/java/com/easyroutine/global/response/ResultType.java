4ac41d3f90e51aeccd3f87324e5e784f4e188a54
lkwoung88 <lkwoung88@gmail.com>
2025-04-30 18:29:23 +0900

    [feat] 공통 응답,예외처리 - 봉투패턴 적용

---
+++ core/api/src/main/java/com/easyroutine/global/response/ResultType.java
@@ -0,0 +1,22 @@
+package com.easyroutine.global.response;
+
+import lombok.Getter;
+
+@Getter
+public enum ResultType {
+    SUCCESS(true, "OK", "성공"),
+    FAIL(false, "ERR-S001" ,"실패"),
+    DATA_NOT_FOUND(false, "ERR-D001" ,"실패"),
+    ILLEGAL_ARGUMENT(false, "ERR-B001" ,"실패"),
+    ;
+
+    private final boolean success;
+    private final String code;
+    private final String message;
+
+    ResultType(boolean success, String code, String message) {
+        this.success = success;
+        this.code = code;
+        this.message = message;
+    }
+}
