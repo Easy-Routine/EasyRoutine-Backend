38707a47b71929a52ebf1867df36d1544dc8eb1c
lkwoung88 <lkwoung88@gmail.com>
2025-05-01 16:02:34 +0900

    [test] test 환경 설정

--- core/api/src/test/java/com/easyroutine/ControllerTestSupport.java
+++ core/api/src/test/java/com/easyroutine/ControllerTestSupport.java
@@ -1,12 +1,15 @@
 package com.easyroutine;
 
 import com.easyroutine.api.controller.ContextController;
+import com.easyroutine.api.controller.v1.member.MemberController;
+import com.easyroutine.domain.member.MemberService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {ContextController.class})
+@WebMvcTest(controllers = {ContextController.class, MemberController.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -14,4 +17,8 @@ public abstract class ControllerTestSupport {
 
     @Autowired
     protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected MemberService memberService;
 }
+
--- core/api/src/test/java/com/easyroutine/IntegrationTestSupport.java
+++ core/api/src/test/java/com/easyroutine/IntegrationTestSupport.java
@@ -2,10 +2,12 @@ package com.easyroutine;
 
 import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
 
 @Tag("context")
 @SpringBootTest
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-public abstract class ContextTest {
+@ActiveProfiles("test")
+public abstract class IntegrationTestSupport {
 }
