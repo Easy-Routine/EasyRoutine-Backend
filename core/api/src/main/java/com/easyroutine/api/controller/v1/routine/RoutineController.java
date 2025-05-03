cd537425992aa9bdcdca688900fe98b6e3a3946e
seonghanim <tjdgksdl1120@gmail.com>
2025-05-03 12:49:56 +0900

    [feat] controller 추가

--- core/api/src/main/java/com/easyroutine/api/controller/v1/routine/RoutineController.java
+++ core/api/src/main/java/com/easyroutine/api/controller/v1/routine/RoutineController.java
@@ -1,8 +1,14 @@
 package com.easyroutine.api.controller.v1.routine;
 
+import com.easyroutine.api.controller.v1.routine.request.RoutineCreateRequest;
+import com.easyroutine.api.security.oauth.CustomOAuth2User;
+import com.easyroutine.domain.routine.RoutineService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/routines")
 @RequiredArgsConstructor
 public class RoutineController {
+    private final RoutineService routineService;
 
+
+    @Operation(summary = "루틴 생성", description = "루틴 생성 API")
+    @PostMapping
+    public Long createRoutine(@AuthenticationPrincipal CustomOAuth2User customOAuth2User, @RequestBody RoutineCreateRequest routineCreateRequest){
+        String memberId = customOAuth2User.getMemberId();
+        routineCreateRequest.getRoutineDto().setMemberIdFromToken(memberId);
+        return routineService.createRoutine(routineCreateRequest.getRoutineDto());
+    }
 }
--- core/api/src/main/java/com/easyroutine/domain/routine/dto/RoutineDto.java
+++ core/api/src/main/java/com/easyroutine/domain/routine/dto/RoutineDto.java
@@ -3,14 +3,23 @@ package com.easyroutine.domain.routine.dto;
 import com.easyroutine.domain.exercises.dto.ExercisesDto;
 import com.easyroutine.domain.routine_exercise.dto.RoutineExerciseDto;
 import lombok.Getter;
+import net.minidev.json.annotate.JsonIgnore;
 
 import java.util.List;
 
 @Getter
 public class RoutineDto {
+    @JsonIgnore
     private Long id;
+    @JsonIgnore
     private String memberId;
+
     private String name;
     private String color;
     private List<RoutineExerciseDto> routineExerciseDtoList;
+
+
+    public void setMemberIdFromToken(String memberId){
+        this.memberId = memberId;
+    }
 }
