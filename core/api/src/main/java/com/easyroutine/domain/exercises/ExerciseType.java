39ee2a49fb2e14e244ce241c0b2f3b7c3122b320
doggopawer <doggopawer@gmail.com>
2025-08-01 19:48:06 +0900

    [fix]: ExerciseType 수정 - REST 타입 추가

--- core/api/src/main/java/com/easyroutine/domain/exercises/ExerciseType.java
+++ core/api/src/main/java/com/easyroutine/domain/exercises/ExerciseType.java
@@ -9,15 +9,19 @@ import java.util.List;
 @Getter
 public enum ExerciseType {
 
-	WEIGHT, COUNT, TIME;
+	WEIGHT, COUNT, REST, TIME;
 
 	public static List<ExerciseType> convertToEnum(List<String> types) {
-		try {
-			return types.stream().map(ExerciseType::valueOf).toList();
-		}
-		catch (IllegalArgumentException e) {
-			throw new BusinessException(ResultType.INPUT_ERROR, "운동 타입 입력이 잘못되었습니다." + types);
-		}
+	try {
+		return types.stream()
+				.map(String::trim)  // 앞뒤 공백 제거
+				.filter(type -> !type.isEmpty()) // 빈 문자열 제거
+				.map(ExerciseType::valueOf)
+				.toList();
+	} catch (IllegalArgumentException e) {
+		throw new BusinessException(ResultType.INPUT_ERROR, "운동 타입 입력이 잘못되었습니다." + types);
 	}
+}
+
 
 }
